4cadb37bd322a429b8d1b2a8a7530bff2e1e81ee
Faris <64802149+izet28@users.noreply.github.com>
2024-04-23 14:06:16 +0700

    Update page.tsx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Web() {
 
   return (
     <div>
-      <h1>Webssss</h1>
+      <h1>Wessbssss</h1>
       <form onSubmit={onSubmit}>
         <label htmlFor="name">Name </label>
         <input
